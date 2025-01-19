f38ece1c1ee3a56b3ff1dab626d9211b4f41d551
Roman <hmrfins@gmail.com>
2025-01-19 18:12:59 +0000

    deps & setup

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ setup(
     name="fastsam",
     version="0.1.1",
     install_requires=REQUIREMENTS,
-    packages=["fastsam", "fastsam_tools"],
+    packages=["fastsam", "fastsam_tools", "ultralytics"],
     package_dir={
         "fastsam": "fastsam",
         "fastsam_tools": "utils",
+        "ultralytics": "ultralytics"
     },
     url="https://github.com/manatees-tech/fastsam_api"
 )
